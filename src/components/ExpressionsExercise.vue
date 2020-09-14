372385103a37c596bba750da187eb5d6df623c3f
Kyle Bennion <bilekennion@hotmail.com>
2020-09-14 14:27:00 -0600

    Expressions Complete

--- src/components/ExpressionsExercise.vue
+++ src/components/ExpressionsExercise.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="expression-exercises container">
-        <div class="row">
-            <div class="col-12">
-                <h1>Exercises</h1>
-                <div class="border p-1">
-                    <p>Write a math equation Vue can evaluate.</p>
-
-                    <p> {{
-                        <!-- Add your code inside the curlies --> }}</p>
-                </div>
-                <div class="border p-1">
-                    <p>Use interpolation to get the customary Hello, World! to display on the screen. Don't
-                        forget your quotes</p>
-                    <p> {{
-                        <!-- Add your code inside the curlies --> }}</p>
-                </div>
-            </div>
+  <div class="expression-exercises container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Exercises</h1>
+        <div class="border p-1">
+          <p>Write a math equation Vue can evaluate.</p>
+          <p>{{ 2113/21+13 }}</p>
         </div>
+        <div class="border p-1">
+          <p>
+            Use interpolation to get the customary Hello, World! to display on the screen. Don't
+            forget your quotes
+          </p>
+          <p>
+            {{"The Prise Wishes You To Watch Over Me."}}
+            <!-- Add your code inside the curlies -->
+          </p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
